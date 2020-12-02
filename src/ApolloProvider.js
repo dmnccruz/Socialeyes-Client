6ff2af31e8c5d19dc8b9f5b0164df116e05fcbe6
dominicmartincruz <dominicmartincruz@gmail.com>
2020-12-02 23:04:11 +0800

    edited proxy and server base url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'https://safe-journey-38464.herokuapp.com/'
 })
 
 const authLink = setContext(() => {
